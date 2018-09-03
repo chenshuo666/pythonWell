eba9962aeb12fc13586bc1ae2a951d1aebe0c65d
SebastianWilliams666 <929387146@qq.com>
2018-09-03 11:30:21 +0800

    tree category

--- cs_data_structure_and_algorithms/tree/tree_category/universal_tree.py
+++ cs_data_structure_and_algorithms/tree/tree_category/universal_tree.py
@@ -1,75 +1,27 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
 # Author:Sebastian Williams
-# import queue
-# import treelib
-#
-# class Node:
-#     def __init__(self,data,first_child=None,next_sibling=None):
-#         self.data = data
-#         self.first_child = first_child
-#         self.next_sibling = next_sibling
-#
-# class UniversalTree(object):
-#     def __init__(self,n):
-#         self.root = None
-#         self.node_number = 0
-#         self.fork = n
-#
-#     def add(self, data):
-#         """create complete n tree"""
-#         global pop_node
-#         count_inner =0
-#         count_outside = 0
-#         node = Node(data)
-#         q = queue.LifoQueue()
-#
-#         if self.root is None:
-#             self.root = node
-#             q.put(self.root)
-#
-#         else:
-#             if count_outside == 0:
-#                 pop_node = q.get()
-#
-#             if count_inner < self.fork:
-#                 if pop_node.next_sibling ==None:
-#                     pop_node.next_sibling = node
-#                     q.put(pop_node.next_sibling)
-#                     count_inner += 1
-#                     return
-#                 else:
-#                     pop_node = pop_node.next_sibling
-#                     pop_node.next_sibling = node
-#                     q.put(pop_node.next_sibling)
-#                     count_inner += 1
-#                     return
-#
-#             elif pop_node.first_child is None:
-#                 pop_node.first_child = node
-#                 q.put(pop_node.first_child)
-#                 return
 
 class node:
     def __init__(self, data):
-        self._data = data
-        self._children = []
+        self.data = data
+        self.children = []
 
     def getdata(self):
-        return self._data
+        return self.data
 
     def getchildren(self):
-        return self._children
+        return self.children
 
     def add(self, node):
         ##if full
-        if len(self._children) == 4:
+        if len(self.children) == 4:
             return False
         else:
-            self._children.append(node)
+            self.children.append(node)
 
     def go(self, data):
-        for child in self._children:
+        for child in self.children:
             if child.getdata() == data:
                 return child
         return None
